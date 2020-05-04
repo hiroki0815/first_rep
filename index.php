5279ab31fce6010e098dab036fd449f2a5bc94ec
hiroki0815 <nagaoka_hiroki_n@yahoo.co.jp>
2020-05-04 15:25:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-var_dump($pdo->query('select sysdate from dual')));
+var_dump($pdo->query('select '1' from dual'));
 
 //phpinfo();
 var_dump($pdo->query('select sysdate from dual'));
