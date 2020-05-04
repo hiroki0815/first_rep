a84c49bce583c0a4f14674d80972b8b6a2219eda
hiroki0815 <nagaoka_hiroki_n@yahoo.co.jp>
2020-05-04 15:32:32 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-var_dump($pdo->query('select '1' from dual'));
+//var_dump($pdo->query('select '1' from dual'));
 
-var_dump($pdo->query('select sysdate from dual'));
+//var_dump($pdo->query('select sysdate from dual'));
 
-echo '終わり<br>';
+//echo '終わり<br>';
+
+var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+
 phpinfo();
 ?>
