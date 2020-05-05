d0907ca2fe3e0c21b549b363efa1200445846d6c
Hiroki Nagaoka <nagaoka_hiroki_n@yahoo.co.jp>
2020-05-05 21:57:34 +0900

    Update index.php

--- firstcake2/index.php
+++ firstcake2/index.php
@@ -1,31 +1,16 @@
-<?
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
 
-$dbinfo = parse_url(getenv('DATABASE_URL'));
-$dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
-$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-
-echo '<head>';
-//    <meta charset="utf-8">
-echo '<title>test page</title>';
-echo '</head>';
-echo '<body>';
- $qry = $pdo->prepare('select * from staff;');
- $qry->execute();
- foreach($qry->fetchAll() as $row){
- 	// 取り出したデータの処理
- 	echo '<tr>';
- 	echo '<td>', $row['id'], '</td>';
- 	echo '<td>', $row['name'], '</td>';
- 	echo '</tr>';
- }
-
-echo '</body>';
-echo '修正5<br>';
-//var_dump($row);
-
-//pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-
-//var_dump($pdo->query($sql));
-
-phpinfo();
-?>
+require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
